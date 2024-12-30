c3a4864c76e60c37450eb1e4297f4b2ef5ab2c34
HyuckJuneHong <jjj8337721@naver.com>
2024-12-31 01:38:37 +0900

    feat: todo list ui generate

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -3,13 +3,36 @@ import React, {Component} from "react";
 import "./App.css";
 
 export default class App extends Component {
-    // 함수형 컴포넌트는 없어도 되지만 클래스 컴포넌트애서는 render()를 사용해서 JSX를 반환해야하기 때문에 render() 사용
+    btnStyle = {
+        color: "#fff",
+        border: "none",
+        padding: "4px 7px",
+        borderRadius: "50%",
+        cursor: "pointer",
+        float: "right"
+    }
+
+    getStyle = () => {
+        return {
+            padding: "10px",
+            borderBottom: "1px #ccc dotted", // 속성의 하단 테두리 설정: 테두리 두깨, 색상, 점선 테두리
+            textDecoration: "none" // 텍스트에 적용된 장식(점선, 취소선) 제어: 기본 장식 제거
+        }
+    }
+
+    // 함수형 컴포넌트는 없어도 되지만 클래스 컴포넌트애서는 render()를 사용해서 JSX를 반환해야 하기 때문에 render() 사용
     render() {
         return (
             <div className="container">
                 <div className="todoBlock">
                     <div className="title">
-                        <h1>To do list.</h1>
+                        <h1>To do list</h1>
+                    </div>
+
+                    <div style={this.getStyle()}>
+                        <input type="checkbox" defaultChecked={false}/>
+                        React Study
+                        <button style={this.btnStyle}>x</button>
                     </div>
                 </div>
             </div>
