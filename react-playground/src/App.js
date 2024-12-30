e523a9e635bfc95b3013bf629cc0265a4a621b3f
HyuckJuneHong <jjj8337721@naver.com>
2024-12-31 01:26:53 +0900

    feat: todo container generate

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,8 +1,16 @@
-function App() {
-    return (
-        <div>
-        </div>
-    );
-}
+// 리액트 라이브러리에서 컴포넌트를 가져옴. 이는 컴포넌트에서 제공하는 render() 메서드를 사용하기 위함.
+import React, {Component} from "react";
+import "./App.css";
 
-export default App;
+export default class App extends Component {
+    // 함수형 컴포넌트는 없어도 되지만 클래스 컴포넌트애서는 render()를 사용해서 JSX를 반환해야하기 때문에 render() 사용
+    render() {
+        return (
+            <div className="container">
+                <div className="todoBlock">
+                    Hello.
+                </div>
+            </div>
+        )
+    }
+}
