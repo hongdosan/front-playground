f86afb6e3268681a52d4011fefd2d4ae3665caff
HyuckJuneHong <jjj8337721@naver.com>
2025-01-04 02:41:08 +0900

    refactor: App.js tailwind css

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -47,7 +47,7 @@ export default function App() {
          * - mb-3: "margin-bottom: 0.75rem" => 요소의 아래쪽 외부 여백(margin-bottom)을 설정
          */
         <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
-            <div className="w-full p-7 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+            <div className="w-full p-7 m-4 bg-white rounded-2xl shadow lg:w-3/4">
                 <div className="flex justify-between mb-3">
                     <h1>To do list</h1>
                 </div>
